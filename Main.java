f19cf2bc20ca0972da22f78ea5e5eca169d3bf31
Minh Le Duc (Intern-Data&AI-Technology-IVND-HO) <minh.leduc@vndirect.com.vn>
2022-10-31 21:29:57 +0700

    minhdev2 Show ProductCart

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import package2.ProductCart;
+import package2.UserService;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("ham main");
+        ProductCart productCart = new ProductCart();
+        UserService userService = new UserService();
+        productCart.showProduct("192022");
+        userService.showAllUser("minh");
     }
 }
---
+++ package1/Product.java
@@ -0,0 +1,40 @@
+package package1;
+
+public class Product {
+    private String productId;
+    private String productName;
+    private float cost;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId='" + productId + '\'' +
+                ", productName='" + productName + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
